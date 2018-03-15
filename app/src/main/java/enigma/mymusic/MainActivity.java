55d6dfa6fce9811afc0c2289dade9c337e562caf
Joseph <ramirezjantonio@hotmail.com>
2018-03-15 13:28:34 -0600

    arreglo de errores, producto final

--- app/src/main/java/enigma/mymusic/MainActivity.java
+++ app/src/main/java/enigma/mymusic/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -32,8 +33,25 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
     AudioManager audioManager;
     TextView v1,v2,v3,v4;
+    static ArrayList<String> elementos= new ArrayList<>();
+    static ArrayList<Integer> musicId= new ArrayList<>();
+    static ArrayList<Integer> lyricsId= new ArrayList<>();
+    static int pos=0;
     static boolean usuarioCambia= false;
     static boolean termina= false;
+
+    public void next(View view){
+        pos++;
+        if(pos==elementos.size())
+            pos=0;
+        ver(pos);
+    }
+    public void prev(View view){
+        pos--;
+        if(pos<0)
+            pos=elementos.size()-1;
+        ver(pos);
+    }
 
     public void playPauseButton(View view){
         Button button= (Button)view;
@@ -57,16 +75,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mediaPlayer = MediaPlayer.create(this, R.raw.believer);
+        Field[] fields= R.raw.class.getFields();
+        elementos= new ArrayList<String>();
+        try{
+            for(int i=0; i<fields.length;i++){
+                if(!(fields[i].getName().contains("lrc"))){
+                    elementos.add(fields[i].getName().replaceAll("_", " "));
+                    musicId.add(fields[i].getInt(null));}
+                else{
+                    lyricsId.add(fields[i].getInt(null));
+                }
+            }}
+        catch (Exception e){
+
+        }
+        mediaPlayer = MediaPlayer.create(this, musicId.get(0));
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         v1= findViewById(R.id.textView);
         v2= findViewById(R.id.textView2);
         v3= findViewById(R.id.textView3);
         v4= findViewById(R.id.textView4);
-        v4.setText("Believer, Imagine Dragons");
+        v4.setText(elementos.get(0).replaceAll("_"," ").toUpperCase());
         int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-        procesaString(R.raw.believer_lrc);
+        procesaString(lyricsId.get(0));
         SeekBar volumeSeekBar = (SeekBar) findViewById(R.id.volumeSeekBar);
         volumeSeekBar.setProgress(currentVolume);
         volumeSeekBar.setMax(maxVolume);
@@ -128,18 +160,14 @@ public class MainActivity extends AppCompatActivity {
                     public void run(){
                         advanceSeekBar.setProgress(mediaPlayer.getCurrentPosition());
                         if(mediaPlayer.getCurrentPosition()+1000>= mediaPlayer.getDuration()) {
-                            Button button= findViewById(R.id.button2);
-                            button.setBackgroundResource(R.drawable.playbutton1);
-                            mediaPlayer.seekTo(0);
-                            mediaPlayer.pause();
-                            index=0;
-                            termina=false;
-                            v1.setText("");
-                            v2.setText("");
-                            v3.setText(lyrics.get(0).getLetra());
+                            pos++;
+                            if(pos==elementos.size())
+                                pos=0;
+                            ver(pos);
                         }
+                        else{
                         int i=mediaPlayer.getCurrentPosition()/1000;
-                        cambiaLyrics(i);
+                        cambiaLyrics(i);}
                     }
                 });
 
@@ -148,7 +176,6 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView= findViewById(R.id.listView);
 
-        final ArrayList<String> elementos= new ArrayList<String>(asList("So Far Away, Avenged Sevenfold","I Dont Want to Miss a Thing, Aerosmith","Believer, Imagine Dragons", "Dream On, Aerosmith", "Bohemian Rhapsody, Queen"));
         listView.setAdapter(new ArrayAdapter<String>(this, R.layout.my_list, elementos) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
@@ -164,20 +191,19 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                ver(elementos.get(i));
+                pos=i;
+                ver(i);
             }
         });
     }
 
     private void cambiaLyricsPos(int progress) {
-        Log.d("PROGRESOOOOOOOOO", progress + "");
         boolean estado = false;
         int cont=0;
         if(!(progress<lyrics.get(0).getTiempo())) {
             for (int i = 0; i < lyrics.size() && estado == false; i++) {
                 if (lyrics.get(i).getTiempo() > progress) {
                     cont++;
-                    Log.d("TIEMPOOOOOOOOO", lyrics.get(i).getTiempo() + "");
                     if (i != 0) {
                         index = i - 1;
                     }
@@ -268,41 +294,24 @@ public class MainActivity extends AppCompatActivity {
         return true; // super.dispatchKeyEvent(event);
     }
 
-    public void ver(String s){
+    public void ver(int i){
+        String s= elementos.get(i);
         mediaPlayer.stop();
         lyrics= new ArrayList<>();
-        switch (s) {
-            case "So Far Away, Avenged Sevenfold":
-                procesaString(R.raw.so_far_away_lrc);
-                mediaPlayer = MediaPlayer.create(this.getApplicationContext(), R.raw.so_far_away);
-                break;
-            case "Bohemian Rhapsody, Queen":
-                procesaString(R.raw.bohemian_rhapsody_lrc);
-                mediaPlayer = MediaPlayer.create(this.getApplicationContext(), R.raw.bohemian_rhapsody);
-                break;
-            case "Believer, Imagine Dragons":
-                procesaString(R.raw.believer_lrc);
-                mediaPlayer = MediaPlayer.create(this.getApplicationContext(), R.raw.believer);
-                break;
-            case "I Dont Want to Miss a Thing, Aerosmith":
-                procesaString(R.raw.i_dont_want_to_miss_a_thing_lrc);
-                mediaPlayer = MediaPlayer.create(this.getApplicationContext(), R.raw.i_dont_want_to_miss_a_thing);
-                break;
-            default:
-                procesaString(R.raw.dream_on_lrc);
-                mediaPlayer = MediaPlayer.create(this.getApplicationContext(), R.raw.dream_on);
-                break;
-        }
+        procesaString(lyricsId.get(i));
+        mediaPlayer = MediaPlayer.create(this.getApplicationContext(), musicId.get(i));
         v1.setText("");
         v2.setText("");
-        v4.setText(s);
+        v4.setText(s.replaceAll("_"," ").toUpperCase());
         v3.setText(lyrics.get(0).getLetra());
         final SeekBar advanceSeekBar = (SeekBar) findViewById(R.id.advanceSeekBar);
         int duration = mediaPlayer.getDuration();
         int progress = mediaPlayer.getCurrentPosition();
         advanceSeekBar.setMax(duration);
         advanceSeekBar.setProgress(progress);
+        Log.d("SI", lyrics.get(0).getLetra());
         cambiaLyricsPos(0);
+        termina= false;
         mediaPlayer.start();
     }
 }
